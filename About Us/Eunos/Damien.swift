dedfd4d0b79eda760937bf8d43eda0e0c1dbca42
Daluven <damienlau2006@gmail.com>
2022-06-23 13:39:26 +0800

    change

--- About Us/Eunos/Damien.swift
+++ About Us/Eunos/Damien.swift
@@ -10,14 +10,51 @@ import SwiftUI
 struct Damien: View {
     
     var body: some View {
-        ZStack(){
-            Color(red: 50/255, green: 50/255, blue: 100/255)
-            VStack{
-                Text("Hi I'm Damien")
-                    .font(.custom("Atkinson Hyperlegible", size: 24))
-                    .colorInvert()
+        ZStack(alignment: .top){
+            Color("Damien Background Color")
+            Rectangle()
+                .frame(height: 375)
+                .offset(y: -150)
+                .foregroundColor(Color(red: 30/255, green: 30/255, blue: 30/255))
+            Spacer()
+            VStack {
+                HStack {
+                    Image("Daluven pfp")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 180, height: 180)
+                        .mask({ Circle() })
+                        .padding()
+                    VStack(alignment: .leading) {
+                        Text("Hi i'm")
+                            .font(.title2)
+                            .multilineTextAlignment(.leading)
+                        Text("Damien Lau")
+                            .font(.largeTitle)
+                            .bold()
+                            .multilineTextAlignment(.leading)
+                    }
+                }
+                Button {
+                    if let yourURL = URL(string: "https://www.youtube.com/channel/UCGrAugsuGhfpDiHCz3DJhxg") {
+                           UIApplication.shared.open(yourURL, options: [:], completionHandler: nil)
+                       }
+                } label: {
+                    VStack {
+                        Text("Check out my youtube ig")
+                            .font(.title)
+                        Text("(I don't upload regularly)")
+                            .font(.title3)
+                    }
+                }
+                .foregroundColor(.white)
+                .frame(width: 350, height: 100)
+                .background(Color(red: 150/255, green: 30/255, blue: 30/255))
+                .cornerRadius(20)
+                .padding()
             }
         }
+        .background(Color("Damien Background Color"))
     }
 }
 
